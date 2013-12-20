9e8605ca1221066ff7f3f3eedf8b36bbf2d86a48
romancuscenco <r.cuscenco@yandex.ru>
2013-12-20 18:51:06 +0200

    first commit

---
+++ SlidingPuzzle/js/sliding-puzzle.js
@@ -0,0 +1,25 @@
+﻿$(function () {
+    var numberOFPieces = 12,
+        aspect = "3:4",
+        aspectW = parseInt(aspect.split(':')[0]),
+        aspectH = parseInt(aspect.split(':')[1]),
+        container = $("#puzzle"),
+        imgContainer = container.find("figure"),
+        img = imgContainer.find("img"),
+        path = img.attr("src"),
+        piece = $("<div />"),
+        pieceW = Math.floor(img.width() / aspectW),
+        pieceH = Math.floor(img.height() / aspectH),
+        idCounter = 0,
+        positions = [],
+        empty = {
+            top: 0,
+            left: 0,
+            bottom: pieceH,
+            right: pieceW
+        },
+        previous = {},
+        timer,
+        currentTime = {},
+        timerDisplay = container.find("#time").find("span");
+});
