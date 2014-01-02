d1f330d44fd7b544339f6ce5782a3b817f51a07a
romancuscenco <r.cuscenco@yandex.ru>
2014-01-02 23:56:06 +0200

    Added click event listener to map

--- SlidingPuzzle/js/google-map.js
+++ SlidingPuzzle/js/google-map.js
@@ -26,4 +26,30 @@
     api.event.addListener(homeMarker, "click", function () {
         infoWindow.open(map, homeMarker);
     });
+
+    var addMarker = function (e) {
+        if (clicks <= 1) {
+            positions.push(e.latLng);
+
+            var marker = new api.Marker({
+                map: map,
+                position: e.latLng,
+                flat: (clicks === 0) ? true : false,
+                animation: api.Animation.DROP,
+                title: (clicks === 0) ? "Start" : "",
+                icon: (clicks === 0) ? "img/start.png" : "",
+                draggable: true,
+                id: (clicks === 0) ? "Start" : "End"
+            });
+
+            api.event.trigger(map, "locationAdd", e);
+        }
+        else {
+            api.event.removeListener(mapClick);
+            return false;
+        }
+    }
+
+    var mapClick = api.event.addListener(map, "click", addMarker);
+
 });
