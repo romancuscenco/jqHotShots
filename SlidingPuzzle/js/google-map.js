7f13c720ca7b6a232af9aeb21eeb0e7a61dc9cbd
romancuscenco <r.cuscenco@yandex.ru>
2014-01-03 15:45:04 +0200

    Done Google Map

--- SlidingPuzzle/js/google-map.js
+++ SlidingPuzzle/js/google-map.js
@@ -42,14 +42,108 @@
                 id: (clicks === 0) ? "Start" : "End"
             });
 
+            api.event.addListener(marker, "dragend", markerDrag);
+
             api.event.trigger(map, "locationAdd", e);
         }
         else {
             api.event.removeListener(mapClick);
             return false;
-        }
+        }        
     }
 
     var mapClick = api.event.addListener(map, "click", addMarker);
+
+    api.event.addListener(map, "locationAdd", function (e) {
+        var journeyEl = $("#journey"),
+            outer = (journeyEl.length) ? journeyEl : $("<div />", { id: "journey" });
+
+        new api.Geocoder().geocode({ "latLng": e.latLng }, function (results) {
+            $("<h3 />", { text: (clicks === 0) ? "Start" : "End" }).appendTo(outer);
+            $("<p />", {
+                text: results[0].formatted_address,
+                id: (clicks === 0) ? "StartPoint" : "EndPoint",
+                "data-latLng": e.latLng
+            }).appendTo(outer);
+
+            if (!journeyEl.length) {
+                outer.appendTo(ui);
+            } else {
+                $("<button />", {
+                    id: "getQuote",
+                    text: "Get quote"
+                }).prop("disabled", true).appendTo(journeyEl);
+            }
+
+            clicks++;
+        });
+    });
+
+    var markerDrag = function (e) {
+        var elId = ["#", this.get("id"), "Point"].join("");
+
+        new api.Geocoder().geocode({ "latLng": e.latLng }, function (results) {
+            $(elId).text(results[0].formatted_address);
+        });
+    };
+
+    $("#weight").on("keyup", function () {
+        if (timeout) {
+            clearTimeout(timeout);
+        }
+
+        var field = $(this),
+            enableButton = function () {
+                if (field.val()) {
+                    $("#getQuote").removeProp("disabled");
+                } else {
+                    $("#getQuote").prop("disabled", true);
+                }
+            },
+            timeout = setTimeout(enableButton, 250);
+    });
+
+    $("body").on("click", "#getQuote", function (e) {
+        e.preventDefault();
+        $(this).remove();
+
+        new api.DistanceMatrixService().getDistanceMatrix({
+            origins: [$("#StartPoint").attr("data-latLng")],
+            destinations: [$("#EndPoint").attr("data-latLng")],
+            travelMode: google.maps.TravelMode.DRIVING,
+            unitSystem: google.maps.UnitSystem.IMPERIAL
+        },
+        function (response) {
+            var list = $("<dl />", { "class": "clearfix", id: "quote" }),
+                format = function (number) {
+                    var rounded = Math.round(number * 100) / 100,
+                        fixed = rounded.toFixed(2);
+
+                    return fixed;
+                },
+                term = $("<dt />"),
+                desc = $("<dd />"),
+                distance = response.rows[0].elements[0].distance,
+                weight = $("#weight").val(),
+                distanceString = distance.text + "les",
+                distanceNum = parseFloat(distance.text.split(" ")[0]),
+                distanceCost = format(distanceNum * 3),
+                weightCost = format(distanceNum * 0.25 * distanceNum),
+                totalCost = format(+distanceCost + +weightCost);
+                
+            $("<h3 />", { text: "Your quote", id: "quoteHeading" }).appendTo(ui);
+
+            term.clone().html("Distance:").appendTo(list);
+            desc.clone().text(distanceString).appendTo(list);
+            term.clone().text("Distance cost:").appendTo(list);
+            desc.clone().text("$" + distanceCost).appendTo(list);
+            term.clone().text("Weight cost:").appendTo(list);
+            desc.clone().text("$" + weightCost).appendTo(list);
+            term.clone().addClass("total").text("Total:").appendTo(list);
+            desc.clone().addClass("total").text("$" + totalCost).appendTo(list);
+
+            list.appendTo(ui);
+        });
+    });
 
 });
